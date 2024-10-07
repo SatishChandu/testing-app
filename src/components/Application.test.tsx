a9146f02ef00d73f7ef305c72ea6fd55168e6e3c
satish2596 <satish.chandu@cmsedge.com>
2024-10-07 15:57:48 +0530

    formatted the test file

--- src/components/Application.test.tsx
+++ src/components/Application.test.tsx
@@ -52,9 +52,5 @@ test("renders focus while clicking tab button", async () => {
   await userEvent.tab();
   expect(amountInput).toHaveFocus();
   await userEvent.tab();
-
-
-
-
   expect(setButton).toHaveFocus();
 });
