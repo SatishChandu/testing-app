c39de25ae3aefc9d44faae1ef0f3546f0d896c08
satish2596 <satish.chandu@cmsedge.com>
2024-10-08 17:30:57 +0530

    deleted the user component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { MuiMode } from "./components/mui/mui-mode";
 import { AppProviders } from "./components/providers/app-providers";
 import "./App.css";
 import { CounterTwo } from "./components/counter-two/counter-two";
-// import { User } from './components/user/user';
 import { Application } from "./components/Application";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         {/* <Application /> */}
         <MuiMode />
         {/* <CounterTwo count={0} handleIncrement={() => {}} handleDecrement={() => {}} /> */}
-        {/* <User /> */}
       </div>
     </AppProviders>
   );
